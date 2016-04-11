f3c9577881ead1d6da35d0aa40dd4f079447845b
Charly <charly.soft@hotmail.com>
2016-04-11 15:41:36 -0500

    Add functions and views

--- links/products.php
+++ links/products.php
@@ -35,35 +35,44 @@
     }
   } else if ($operation === 'modify') {
     $query = "UPDATE products SET";
+    if ($_REQUEST['barcode']) {
+      $query .= " barcode='" . $_REQUEST['barcode'] . "',";
+    }
     if ($_REQUEST['name']) {
       $query .= " name='" . $_REQUEST['name'] . "',";
     }
-    if ($_REQUEST['address']) {
-      $query .= " address='" . $_REQUEST['address'] . "',";
+    if ($_REQUEST['description']) {
+      $query .= " description='" . $_REQUEST['description'] . "',";
     }
-    if ($_REQUEST['colony']) {
-      $query .= " colony='" . $_REQUEST['colony'] . "',";
+    if ($_REQUEST['key_']) {
+      $query .= " key_='" . $_REQUEST['key_'] . "',";
     }
-    if ($_REQUEST['state']) {
-      $query .= " state='" . $_REQUEST['state'] . "',";
+    if ($_REQUEST['brand']) {
+      $query .= " brand='" . $_REQUEST['brand'] . "',";
     }
-    if ($_REQUEST['phone']) {
-      $query .= " phone='" . $_REQUEST['phone'] . "',";
+    if ($_REQUEST['model']) {
+      $query .= " model='" . $_REQUEST['model'] . "',";
     }
-    if ($_REQUEST['contact_name']) {
-      $query .= " contact_name='" . $_REQUEST['contact_name'] . "',";
+    if ($_REQUEST['measure']) {
+      $query .= " measure='" . $_REQUEST['measure'] . "',";
     }
-    if ($_REQUEST['rfc']) {
-      $query .= " rfc='" . $_REQUEST['rfc'] . "',";
+    if ($_REQUEST['treadware']) {
+      $query .= " treadware='" . $_REQUEST['treadware'] . "',";
     }
-    if ($_REQUEST['pc']) {
-      $query .= " pc='" . $_REQUEST['pc'] . "',";
+    if ($_REQUEST['load_index']) {
+      $query .= " load_index='" . $_REQUEST['load_index'] . "',";
     }
-    if ($_REQUEST['city']) {
-      $query .= " city='" . $_REQUEST['city'] . "',";
+    if ($_REQUEST['load_speed']) {
+      $query .= " load_speed='" . $_REQUEST['load_speed'] . "',";
     }
-    if ($_REQUEST['cell_phone']) {
-      $query .= " cell_phone='" . $_REQUEST['cell_phone'] . "',";
+    if ($_REQUEST['retail_price']) {
+      $query .= " retail_price='" . $_REQUEST['retail_price'] . "',";
+    }
+    if ($_REQUEST['wholesale_price']) {
+      $query .= " wholesale_price='" . $_REQUEST['wholesale_price'] . "',";
+    }
+    if ($_REQUEST['special_price']) {
+      $query .= " special_price='" . $_REQUEST['special_price'] . "',";
     }
     $query .= " last_date=now() WHERE id=" . $_REQUEST['id'] . ";";
     $result = mysql_query($query) or die ('Consulta fallida: ' . mysql_error());
@@ -73,18 +82,58 @@
     $query .= "DELETE FROM products WHERE id=" . $_REQUEST['id'] . ";";
     $result = mysql_query($query) or die ('Consulta fallida: ' . mysql_error());
     echo 'Eliminado correctamente.';
-  } else if ($operation === 'products') {
-    $query = "SELECT id, name, barcode FROM products;";
+  }  else if ($operation === 'browserName') {
+    $query = "SELECT * FROM products WHERE name LIKE '%" . $_REQUEST['content'] . "%';";
     $result = mysql_query($query) or die ('Consulta fallida: ' . mysql_error());
+    $print = '<table class="table table-striped tablet-tools">
+      <thead>
+        <tr>
+          <th>Código de Barras</th>
+          <th>Clave</th>
+          <th>Nombre</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>';
     while ($row = mysql_fetch_assoc($result)) {
-      echo $row['id'] . ',' . $row['name'] . ',' . $row['barcode'];
+      $print .= '<tr>
+            <td>' . $row['barcode'] . '</td>
+            <td>' . $row['key_'] . '</td>
+            <td>' . $row['name'] . '</td>
+            <td><button onClick="onClickSelect(' . $row['id'] . ')" type="button" class="btn btn-primary btn-square">Modificar</button></td>
+          </tr>';
     }
-  } else if ($operation === 'selectClient') {
+    $print .= '</tbody></table>';
+    echo $print;
+  } else if ($operation === 'browserBarcode') {
+    $query = "SELECT * FROM products WHERE barcode LIKE '%" . $_REQUEST['content'] . "%';";
+    $result = mysql_query($query) or die ('Consulta fallida: ' . mysql_error());
+    $print = '<table class="table table-striped tablet-tools">
+      <thead>
+        <tr>
+          <th>Código de Barras</th>
+          <th>Clave</th>
+          <th>Nombre</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>';
+    while ($row = mysql_fetch_assoc($result)) {
+      $print .= '<tr>
+            <td>' . $row['barcode'] . '</td>
+            <td>' . $row['key_'] . '</td>
+            <td>' . $row['name'] . '</td>
+            <td><button onClick="onClickSelect(' . $row['id'] . ')" type="button" class="btn btn-primary btn-square">Modificar</button></td>
+          </tr>';
+    }
+    $print .= '</tbody></table>';
+    echo $print;
+  } else if ($operation === 'selectProduct') {
     $id = $_REQUEST['id'];
     $query = "SELECT * FROM products where id=" . $id . ";";
     $result = mysql_query($query) or die ('Consulta fallida: ' . mysql_error());
     $row = mysql_fetch_assoc($result);
-    echo $row['name'] . ',' . $row['address'] . ',' . $row['colony'] . ',' . $row['state'] . ',' . $row['phone'] . ',' . $row['contact_name'] . ',' . $row['rfc'] . ',' . $row['pc'] . ',' . $row['city'] . ',' . $row['cell_phone'];
+    echo $row['barcode'] . ',' . $row['name'] . ',' . $row['description'] . ',' . $row['key_'] . ',' . $row['brand'] . ',' . $row['model'] . ',' . $row['measure'] . ',' . $row['treadware'] . ',' . $row['load_index'] . ',' . $row['load_speed'] . ',' . $row['retail_price'] . ',' . $row['wholesale_price'] . ',' . $row['special_price'];
   }
   mysql_close($link);
  ?>
